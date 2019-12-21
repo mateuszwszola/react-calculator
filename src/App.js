ae5c84a3a0b49bf1eae29f17f49b7b182d19d2a6
Mateusz Wszola <wszola.m@gmail.com>
2019-12-21 21:48:26 +0100

    Add styled header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components/macro';
+import tw from 'tailwind.macro';
 import './App.css';
+
+const Header = styled.header`
+  ${tw`bg-black min-h-screen flex flex-col items-center justify-center text-xl text-white`};
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div css={tw`text-center`}>
+      <Header>
+        <p css={tw`text-blue-300`}>
+          Using <code>tailwind</code> and <code>styled-components</code>{' '}
+          together
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </Header>
     </div>
   );
 }
