f68e7a71c44738fb5860f4c84be4284e0128d433
Mateusz Wszola <wszola.m@gmail.com>
2019-12-21 23:25:42 +0100

    Update tailwind config again to reduce css build size

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 const tailwindcss = require('tailwindcss');
 const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: ['./src/**/*.js'],
+  content: ['./src/**/*.js', './public/index.html'],
 
   // Include any special characters you're using in this regular expression
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
--- src/App.js
+++ src/App.js
@@ -1,44 +1,13 @@
 import React from 'react';
+import Button from './components/Button';
 
 function App() {
   return (
-    <div className="App">
-      <div className="w-full max-w-md bg-gray-800">
-        <form action="" className=" bg-white shadow-md rounded px-8 py-8 pt-8">
-          <div className="px-4 pb-4">
-            <label htmlFor="email" className="text-sm block font-bold  pb-2">
-              EMAIL ADDRESS
-            </label>
-            <input
-              type="email"
-              name="email"
-              id=""
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300 "
-              placeholder="Johnbull@example.com"
-            />
-          </div>
-          <div className="px-4 pb-4">
-            <label htmlFor="password" className="text-sm block font-bold pb-2">
-              PASSWORD
-            </label>
-            <input
-              type="password"
-              name="email"
-              id=""
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300"
-              placeholder="Enter your password"
-            />
-          </div>
-          <div>
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
-            >
-              Sign In
-            </button>
-          </div>
-        </form>
-      </div>
+    <div className="flex flex-col w-3/4 mx-auto my-12 items-center">
+      <h1>Super cool page</h1>
+      <Button onClick={() => console.log('I was clicked')}>
+        I am a button
+      </Button>
     </div>
   );
 }
