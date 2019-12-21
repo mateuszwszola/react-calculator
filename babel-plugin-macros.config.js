b383a38c5382f948ce04accfe164951847841aac
Mateusz Wszola <wszola.m@gmail.com>
2019-12-21 22:56:48 +0100

    Change tailwind config

--- babel-plugin-macros.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  tailwind: {
-    config: './src/tailwind.config.js',
-    styled: 'styled-components/macro'
-  }
-};
---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+const tailwindcss = require('tailwindcss');
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./src/**/*.js'],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
+module.exports = {
+  plugins: [
+    tailwindcss('./tailwind.config.js'),
+    require('autoprefixer'),
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
+  ]
+};
