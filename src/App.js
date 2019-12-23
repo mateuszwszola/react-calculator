fa29b5c0d668cea68baff2df53b816543ff1591a
Mateusz Wszola <wszola.m@gmail.com>
2019-12-23 15:48:27 +0100

    Make footer flex col on mobile

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
       <main className="flex-grow w-full flex items-start sm:items-center">
         <Calculator />
       </main>
-      <footer className="flex justify-around items-center text-center px-4 py-1 bg-gray-900 text-indigo-200 tracking-wide font-light text-lg">
+      <footer className="flex flex-col items-center justify-center sm:flex-row sm:justify-around  text-center px-4 py-1 bg-gray-900 text-indigo-200 tracking-wide font-light sm:text-lg">
         <a
-          className="block mt-1 px-2 py-1 hover:text-indigo-300"
+          className="block mt-1 sm:mt-0 px-2 py-1  hover:text-indigo-300"
           href="https://github.com/mateuszwszola"
           target="_blank"
           rel="noopener noreferrer"
@@ -17,7 +17,7 @@ function App() {
           Mateusz Wszola
         </a>
         <a
-          className="block mt-1 px-2 py-1 hover:text-indigo-300"
+          className="block mt-1 sm:mt-0 px-2 py-1  hover:text-indigo-300"
           href="https://github.com/mateuszwszola/react-calculator-v2"
           target="_blank"
           rel="noopener noreferrer"
